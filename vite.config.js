a34a8f9979b55e182ad66e6b8922a2a679393514
Suyog Lamichhane <Suyoglamichhane86@gmail.com>
2026-07-10 22:39:31 +0545

    frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts', '.json'],
+    // This helps Vite prefer .jsx over .css
+    mainFields: ['module', 'main'],
+  },
+  css: {
+    modules: {
+      // If using CSS modules
+      localsConvention: 'camelCase',
+    },
+  },
 })
